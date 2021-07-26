88bef624763faf57995596753ef2454d961d27db
Jangbe <jangebe91@gmail.com>
2021-07-26 20:52:21 +0700

    sdbfskj

--- app/src/main/java/com/smkypc/siagacovid/GameActivity.java
+++ app/src/main/java/com/smkypc/siagacovid/GameActivity.java
@@ -9,6 +9,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.text.format.Formatter;
 import android.util.Log;
+import android.view.View;
 import android.webkit.PermissionRequest;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -55,19 +56,37 @@ public class GameActivity extends AppCompatActivity {
         webSettings.setAllowFileAccessFromFileURLs(true);
         webSettings.setAllowFileAccess(true);
         webSettings.setAllowUniversalAccessFromFileURLs(true);
-        myWebView.addJavascriptInterface(new WebAppInterface(this), "App");
+//        myWebView.addJavascriptInterface(new WebAppInterface(this), "App");
 
-        String ip = "";
-        try {
-            server = new MyHTTPD(this);
-            server.start();
+//        String ip = "";
+//        try {
+//            server = new MyHTTPD(this);
+//            server.start();
+//
+//            WifiManager wm = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+//            ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress()) + ":" + MyHTTPD.PORT;
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
-            WifiManager wm = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
-            ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress()) + ":" + MyHTTPD.PORT;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        myWebView.loadUrl("http://0.0.0.0:8765/game/index.html");
+        hideSystemUI();
+    }
 
-        myWebView.loadUrl(ip+"/game/index.html");
+    private void hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
